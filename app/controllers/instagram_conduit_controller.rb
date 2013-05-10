9a20016f60354bd2412404901c0630ee16254856
kilen <kilen@gitcafe.com>
2013-05-10 14:18:08 +0800

    verify updates from instagram

--- app/controllers/instagram_conduit_controller.rb
+++ app/controllers/instagram_conduit_controller.rb
@@ -1,7 +1,10 @@
 class InstagramConduitController < ApplicationController
+  CLIENT_ID = "5a97d503088d45a8b0ba147771de3787"
+  CLIENT_SECRET = "94078e484b09455f8936a3a8658d5ee2"
+
   def feed_reciver
-    data = ActiveSupport::JSON.decode request.body
-    media = Media.create!(receives: data.to_json)
+    if updates_from_instagram?
+    end
     render text: "Thank you"
   end
 
@@ -12,5 +15,14 @@ class InstagramConduitController < ApplicationController
 
   def handle_instagram_validation
     render text: params["hub.challenge"] || "success"
+  end
+
+  private 
+
+  def updates_from_instagram?
+    json = request.body.read
+    @json = MultiJson.decode json
+    digest = OpenSSL::Digest::Digest.new 'sha1'
+    OpenSSL::HMAC.hexdigest(digest, CLIENT_SECRET, json) == request.headers["X-Hub-Signature"]
   end
 end
