42ce67e4d4233d5a3d4097d763acaf349c9d9b01
kilen <kilen@gitcafe.com>
2013-05-13 14:12:32 +0800

    add ajax requet new photo route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ NYStreetagram::Application.routes.draw do
   post "/callback" => "instagram_conduit#feed_receiver"
   get "/callback" => "instagram_conduit#handle_instagram_validation"
   get "/show_receives" => "instagram_conduit#show_receives"
+  get "/fetch_new_photos" => "instagram_conduit#new_photos"
 
 end
