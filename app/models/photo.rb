cd61624ebb1d44976792a50ccea63834bba56662
kilen <kilen@gitcafe.com>
2013-05-31 17:03:39 +0800

    remove earilier photos when record exceed specific number

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,8 @@
 class Photo < ActiveRecord::Base
   attr_accessible :info
+  before_create :ensure_photo_number_within_specific_range
+
+  MAX_PHOTO = 5000
 
   def info= obj
     self[:info] = obj.to_json
@@ -11,5 +14,17 @@ class Photo < ActiveRecord::Base
   
   def last(num)
     Photo.find_by_sql("SELECT * FROM clients ORDER BY clients.id DESC LIMIT #{num}")
+  end
+
+  private
+
+  def ensure_photo_number_within_specific_range
+    if Photo.count >= MAX_PHOTO
+      remove_earlier_photos(Photo.count - MAX_PHOTO)
+    end
+  end
+
+  def remove_earlier_photos(num)
+    Photo.order("created_at ASC").limit(num).destroy_all
   end
 end
